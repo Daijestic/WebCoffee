96fac99198738bbf946c612eeda34241043c09c1
Daijestic <daibui1208@gmail.com>
2025-03-31 13:29:15 +0700

    Delete

--- src/main/java/com/javaweb/api/MonAPI.java
+++ src/main/java/com/javaweb/api/MonAPI.java
@@ -3,9 +3,7 @@ package com.javaweb.api;
 import com.javaweb.model.MonDTO;
 import com.javaweb.service.MonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,5 +16,13 @@ public class MonAPI {
     @GetMapping("/api/mon")
     public List<MonDTO> testAPI(@RequestParam(name = "name") String name) {
         return monService.findAll(name);
+    }
+
+    @DeleteMapping("/api/mon/{id}")
+    public void delete(@PathVariable Long id) {
+        MonDTO monDTO = monService.findById(id);
+        if (monDTO != null) {
+            monService.deleteById(id);
+        }
     }
 }
