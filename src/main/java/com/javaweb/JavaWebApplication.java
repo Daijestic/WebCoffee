72c899d8faa79ade36a4099b0945c1f461e18183
Daijestic <daibui1208@gmail.com>
2025-04-08 10:19:03 +0700

    update

--- src/main/java/com/javaweb/JavaWebApplication.java
+++ src/main/java/com/javaweb/JavaWebApplication.java
@@ -6,6 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavaWebApplication {
 
+	/**
+	 * Main method to run the Spring Boot application.
+	 *
+	 * @param args command line arguments
+	 */
+
 	public static void main(String[] args) {
 		SpringApplication.run(JavaWebApplication.class, args);
 	}
